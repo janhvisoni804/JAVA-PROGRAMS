4b56ca589790e6f5624ee08c5f9d5727247ef30e
Janhvi Soni <janhvisoni804@gmail.com>
2026-06-05 21:15:23 +0530

    add solution to move all zeros to the end of array
    Time Complexity-
    we have used 3 for loops therefore the time complexity is O(N)
    
    Space Complexity-
    we haven't used any temperory array to store non-zero elements , we had swaped the elements directly in the original array therefore the space complexity is O(1)

--- Array in Java/zeros_at_end.java
+++ Array in Java/zeros_at_end.java
@@ -1,1 +1,28 @@
-
+public class zeros_at_end{
+    public static void main(String[] args) {
+        int arr[]={1,0,2,3,2,0,0,4,5,1};
+        int first_zero=-1;
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]==0){
+                first_zero=i;
+                break;
+            }
+        }
+        if(first_zero==-1){
+            System.out.println("no zero elements");
+        }
+        else{
+            for(int i=first_zero+1;i<=arr.length-1;i++){
+                if(arr[i]!=0){
+                    int temp=arr[i];
+                    arr[i]=arr[first_zero];
+                    arr[first_zero]=temp;
+                    first_zero++;
+                }
+            }
+            for(int i=0;i<=arr.length-1;i++){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
